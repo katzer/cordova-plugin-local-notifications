9a0ca7d16c09d27b3ba7ecf77387985b76e685f3
Sebastián Katzer <katzer.sebastian@googlemail.com>
2013-09-01 10:23:58 +0200

    Datum wird korrekt berechnet

--- src/android/LocalNotificationOptions.java
+++ src/android/LocalNotificationOptions.java
@@ -26,36 +26,27 @@ public class LocalNotificationOptions {
     private String subTitle       = "";
     private String ticker         = "";
     private boolean repeatDaily   = false;
-    private String notificationId = "";
+    private String notificationId = null;
 
     /**
      * Parse options passed from javascript part of this plugin.
      */
     public void parse (JSONObject options) {
-	    // Parse string representing the date
         String date    = options.optString("date");
         String message = options.optString("message");
 
-	    if (!"".equals(date)) {
-    		String[] dateParts = date.split("/");
+        if (!"".equals(date)) {
+            calendar.setTimeInMillis(1000*Long.parseLong(date));
+        }
 
-            int month = Integer.parseInt(dateParts[0]);
-            int day   = Integer.parseInt(dateParts[1]);
-            int year  = Integer.parseInt(dateParts[2]);
-            int hour  = Integer.parseInt(dateParts[3]);
-            int min   = Integer.parseInt(dateParts[4]);
+        if (!"".equals(message)) {
+            String lines[] = message.split("\\r?\\n");
 
-    		calendar.set(year, month, day, hour, min);
-	    }
+            title = lines[0];
 
-	    if (!"".equals(message)) {
-    		String lines[] = message.split("\\r?\\n");
-
-    		title = lines[0];
-
-    		if (lines.length > 1)
-    		    subTitle = lines[1];
-	    }
+            if (lines.length > 1)
+                subTitle = lines[1];
+        }
 
         ticker         = options.optString("ticker");
         repeatDaily    = options.optBoolean("repeatDaily");
@@ -66,38 +57,38 @@ public class LocalNotificationOptions {
      * Gibt den Kalender mit dem Datum der nächsten Notification an.
      */
     public Calendar getCalendar() {
-	   return calendar;
+       return calendar;
     }
 
     /**
      * Gibt den Titel der Notification an.
      */
     public String getTitle () {
-	   return title;
+       return title;
     }
 
     /**
      * Gibt den Untertitel der Notification an.
      */
     public String getSubTitle () {
-	   return subTitle;
+       return subTitle;
     }
 
     /**
      * Gibt den Ticker der Notification an.
      */
     public String getTicker () {
-	   return ticker;
+       return ticker;
     }
 
     public boolean isRepeatDaily () {
-	   return repeatDaily;
+       return repeatDaily;
     }
 
     /**
      * Gibt die Callback-ID des PluginResults an.
      */
     public String getNotificationId () {
-	   return notificationId;
+       return notificationId;
     }
 }
