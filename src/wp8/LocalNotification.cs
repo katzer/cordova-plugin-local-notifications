24360db01005ba68a685ada18b807af413dadd32
Sebastián Katzer <katzer.sebastian@googlemail.com>
2013-10-21 16:23:50 +0200

    Initial start with WP8 support

---
+++ src/wp8/LocalNotification.cs
@@ -0,0 +1,25 @@
+/**
+ *  LocalNotification.cs
+ *  Cordova LocalNotification Plugin
+ *
+ *  Created by Sebastian Katzer (github.com/katzer) on 07/10/2013.
+ *  Copyright 2013 Sebastian Katzer. All rights reserved.
+ *  GPL v2 licensed
+ */
+
+using System;
+using System.Linq;
+
+using Microsoft.Phone.Shell;
+
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+using WPCordovaClassLib.Cordova.JSON;
+
+namespace Cordova.Extension.Commands
+{
+    public class LocalNotification : BaseCommand
+    {
+
+    }
+}
