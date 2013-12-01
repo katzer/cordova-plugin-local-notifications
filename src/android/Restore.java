d4fcd269e6a74e4717d83fa4a2fc7e6f871937f1
Sebastián Katzer <katzer.sebastian@googlemail.com>
2013-12-01 19:54:16 +0100

    Correct infos.

--- src/android/Restore.java
+++ src/android/Restore.java
@@ -1,8 +1,7 @@
 /**
- *  LocalNotificationRestore.java
  *  Cordova LocalNotification Plugin
  *
- *  Created by Sebastian Katzer (github.com/katzer) on 31/08/2013.
+ *  Created by Sebastian Katzer (github.com/katzer).
  *  Copyright 2013 Sebastian Katzer. All rights reserved.
  *  LGPL v2.1 licensed
  */
