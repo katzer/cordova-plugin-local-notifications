7727d221d1f9a998170925acc8dd09bea063e2c6
Alice Zhang <alicezhang@geotab.com>
2016-06-06 12:06:38 -0400

    tweaked utils to include priority

--- www/local-notification-util.js
+++ www/local-notification-util.js
@@ -69,6 +69,7 @@ exports.applyPlatformSpecificOptions = function () {
         defaults.autoClear = true;
         defaults.led       = undefined;
         defaults.color     = undefined;
+        defaults.priority  = 0;
         break;
     }
 
