1184a027c4625649bff593cb94fe3918e31034b5
Vitor Oliveira <vmfoliveira89@gmail.com>
2017-08-17 23:25:02 +0100

    Add Support for Android O - on the new version to post notifications it must be defined a channel
    It was defined a default channel once there is not yet support to define channels from JS API

--- src/android/notification/Notification.java
+++ src/android/notification/Notification.java
@@ -25,11 +25,13 @@ package de.appplant.cordova.plugin.notification;
 
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
@@ -43,6 +45,8 @@ import java.util.Date;
  * like show, delete, cancel for a single local notification instance.
  */
 public class Notification {
+
+    private final static String CHANNEL_ID = "androidId";
 
     // Used to differ notifications by their life cycle state
     public enum Type {
@@ -93,7 +97,9 @@ public class Notification {
         this.context = context;
         this.options = options;
         this.builder = builder;
-
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            this.builder.setChannelId(CHANNEL_ID);
+        }
         this.receiver = receiver != null ? receiver : defaultReceiver;
     }
 
@@ -334,8 +340,25 @@ public class Notification {
      * Notification manager for the application.
      */
     private NotificationManager getNotMgr () {
-        return (NotificationManager) context
+        NotificationManager notificationManager = (NotificationManager) context
                 .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // The id of the channel.
+            // The user-visible name of the channel.
+            CharSequence name = CHANNEL_ID;
+            // The user-visible description of the channel.
+            String description = CHANNEL_ID;
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            // Configure the notification channel.
+            mChannel.setDescription(description);
+            mChannel.enableLights(true);
+            mChannel.enableVibration(true);
+            //mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            notificationManager.createNotificationChannel(mChannel);
+        }
+        return notificationManager;
     }
 
     /**
