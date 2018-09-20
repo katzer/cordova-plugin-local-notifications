f908e71ce21ff5fbfd44b364838e3f609d101bdd
Keyur Shah <34612659+shahkeyur@users.noreply.github.com>
2018-09-20 05:39:26 +0530

    Update Manager.java

--- src/android/notification/Manager.java
+++ src/android/notification/Manager.java
@@ -19,6 +19,8 @@
  * limitations under the License.
  */
 
+ // codebeat:disable[TOO_MANY_FUNCTIONS] 
+
 package de.appplant.cordova.plugin.notification;
 
 import android.annotation.SuppressLint;
@@ -416,3 +418,5 @@ public final class Manager {
     }
 
 }
+
+ // codebeat:enable[TOO_MANY_FUNCTIONS] 
