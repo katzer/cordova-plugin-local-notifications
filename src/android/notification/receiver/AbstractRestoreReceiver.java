376871d3bff828e0d4b08deed01fe11f398e3528
GitToTheHub <manuelbeck@fastmail.net>
2019-01-18 17:55:10 +0100

    Fixes Android 8 Crashes after Device Reboot (#1611)
    Fixes https://github.com/katzer/cordova-plugin-local-notifications/issues/1541

--- src/android/notification/receiver/AbstractRestoreReceiver.java
+++ src/android/notification/receiver/AbstractRestoreReceiver.java
@@ -26,6 +26,7 @@ package de.appplant.cordova.plugin.notification.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.UserManager;
 
 import org.json.JSONObject;
 
@@ -57,8 +58,10 @@ abstract public class AbstractRestoreReceiver extends BroadcastReceiver {
     public void onReceive (Context context, Intent intent) {
         String action = intent.getAction();
 
-        if (action.equals(ACTION_BOOT_COMPLETED) && SDK_INT >= 24)
-            return;
+        if (SDK_INT >= 24) {
+          UserManager um = (UserManager) context.getSystemService(UserManager.class);
+          if (um == null || um.isUserUnlocked() == false) return;
+        }
 
         Manager mgr               = Manager.getInstance(context);
         List<JSONObject> toasts = mgr.getOptions();
