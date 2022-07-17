7d81d246498c202e02b047a9c6c20270c5b385f6
Florian Quirin <info@bytemind.de>
2022-07-17 15:32:13 +0200

    Added 'PendingIntent.FLAG_MUTABLE' for API >= 31 to Builder.java

--- src/android/notification/Builder.java
+++ src/android/notification/Builder.java
@@ -398,8 +398,12 @@ public final class Builder {
 
         int reqCode = random.nextInt();
 
+        int flags = FLAG_UPDATE_CURRENT;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            flags = PendingIntent.FLAG_MUTABLE | FLAG_UPDATE_CURRENT;
+        }
         PendingIntent deleteIntent = PendingIntent.getBroadcast(
-                context, reqCode, intent, FLAG_UPDATE_CURRENT);
+                context, reqCode, intent, flags);
 
         builder.setDeleteIntent(deleteIntent);
     }
@@ -427,8 +431,12 @@ public final class Builder {
 
         int reqCode = random.nextInt();
 
+        int flags = FLAG_UPDATE_CURRENT;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            flags = PendingIntent.FLAG_MUTABLE | FLAG_UPDATE_CURRENT;
+        }
         PendingIntent contentIntent = PendingIntent.getService(
-                context, reqCode, intent, FLAG_UPDATE_CURRENT);
+                context, reqCode, intent, flags);
 
         builder.setContentIntent(contentIntent);
     }
