3a807faf22802f3ad4d1c0ceb0799f1381b09066
SuraiyaShiratti <73700033+SuraiyaShiratti@users.noreply.github.com>
2022-09-14 12:24:41 +0530

    Update Builder.java

--- src/android/notification/Builder.java
+++ src/android/notification/Builder.java
@@ -372,9 +372,13 @@ public final class Builder {
         }
 
         int reqCode = random.nextInt();
+        int flags = FLAG_UPDATE_CURRENT;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+         flags = PendingIntent.FLAG_MUTABLE | FLAG_UPDATE_CURRENT;
+        }
 
         PendingIntent deleteIntent = PendingIntent.getBroadcast(
-                context, reqCode, intent, FLAG_UPDATE_CURRENT);
+                context, reqCode, intent, flags);
 
         builder.setDeleteIntent(deleteIntent);
     }
@@ -401,9 +405,13 @@ public final class Builder {
         }
 
         int reqCode = random.nextInt();
+        int flags = FLAG_UPDATE_CURRENT;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+         flags = PendingIntent.FLAG_MUTABLE | FLAG_UPDATE_CURRENT;
+        }
 
         PendingIntent contentIntent = PendingIntent.getService(
-                context, reqCode, intent, FLAG_UPDATE_CURRENT);
+                context, reqCode, intent, flags);
 
         builder.setContentIntent(contentIntent);
     }
